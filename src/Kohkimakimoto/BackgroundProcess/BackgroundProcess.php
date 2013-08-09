90bb00b53da904ac8f9c0414102a6d6e3df5f8e8
kohkimakimoto <kohki.makimoto@gmail.com>
2013-08-09 16:58:55 +0900

    Update

--- src/Kohkimakimoto/BackgroundProcess/BackgroundProcess.php
+++ src/Kohkimakimoto/BackgroundProcess/BackgroundProcess.php
@@ -1,9 +1,18 @@
 <?php
 namespace Kohkimakimoto\BackgroundProcess;
 
-class BackendProcess
+use Symfony\Component\Process\Process;
+
+/**
+ *
+ * @author Kohki Makimoto
+ */
+class BackgroundProcess
 {
+  protected $process;
 
-
-
+  public function __construct($commandline, $cwd = null, array $env = null, $stdin = null, $timeout = 60, array $options = array())
+  {
+    $this->process = new Process($commandline, $cwd, $env, $stdin, $timeout, $options);
+  }
 }
--- tests/Kohkimakimoto/BackgroundProcess/BackgroundProcessTest.php
+++ tests/Kohkimakimoto/BackgroundProcess/BackgroundProcessTest.php
@@ -1,9 +1,12 @@
 <?php
 namespace Test\Kohkimakimoto\BackgroundProcess;
 
+use Kohkimakimoto\BackgroundProcess\BackgroundProcess;
+
 class BackgroundProcessTest extends \PHPUnit_Framework_TestCase
 {
   public function testDefault()
   {
+    $bgProcess = new BackgroundProcess("ls -l");
   }
 }

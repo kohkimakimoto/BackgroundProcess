dc9c7261284ea4ae0a6ff366b613faa5917016e0
kohkimakimoto <kohki.makimoto@gmail.com>
2013-08-13 13:27:08 +0900

    Developing Manager class

---
+++ tests/Kohkimakimoto/BackgroundProcess/BackgroundProcessManagerTest/t1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sleep 1
+echo "aaaa" >> /tmp/BackgroundProcessManager_t1.sh.output
+sleep 1
+echo "aaaa" >> /tmp/BackgroundProcessManager_t1.sh.output
+sleep 1
+echo "aaaa" >> /tmp/BackgroundProcessManager_t1.sh.output
